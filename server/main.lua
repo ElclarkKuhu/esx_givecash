628168da62f1a72feccbfd431ce765aef4e12230
Elclark <elclarkkuhu@gmail.com>
2021-11-05 08:25:27 +0800

    add notifications

--- server/main.lua
+++ server/main.lua
@@ -6,7 +6,13 @@ RegisterCommand('givecash', function(source, args, rawCommand)
         local amount = tonumber(args[2])
         if amount ~= nil then
             xPlayer.removeMoney(amount)
+            TriggerClientEvent('esx:showNotification', source, 'You gave ' .. amount .. '$ to ' .. GetPlayerName(target.source))
             target.addMoney(amount)
+            TriggerClientEvent('esx:showNotification', target.source, 'You received ' .. amount .. '$ from ' .. GetPlayerName(source))
+        else
+            TriggerClientEvent('esx:showNotification', source, 'Invalid amount')
         end
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Invalid player id')
     end
 end, false)

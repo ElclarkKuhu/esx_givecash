d53a10d884286dda1f49e1528a71518025f5aa77
Elclark <elclarkkuhu@gmail.com>
2021-11-05 08:28:26 +0800

    forgot to inclued the distance check

--- server/main.lua
+++ server/main.lua
@@ -5,10 +5,20 @@ RegisterCommand('givecash', function(source, args, rawCommand)
     if target ~= nil then
         local amount = tonumber(args[2])
         if amount ~= nil then
-            xPlayer.removeMoney(amount)
-            TriggerClientEvent('esx:showNotification', source, 'You gave ' .. amount .. '$ to ' .. GetPlayerName(target.source))
-            target.addMoney(amount)
-            TriggerClientEvent('esx:showNotification', target.source, 'You received ' .. amount .. '$ from ' .. GetPlayerName(source))
+            local playerPed = GetPlayerPed(source)
+            local targetPed = GetPlayerPed(tonumber(args[1]))
+
+            local playerCoords = GetEntityCoords(playerPed)
+            local targetCoords = GetEntityCoords(targetPed)
+
+            if #(playerCoords - targetCoords) < config.range then
+                local money = tonumber(args[2])
+
+                xPlayer.removeMoney(amount)
+                TriggerClientEvent('esx:showNotification', source, 'You gave ' .. amount .. '$ to ' .. GetPlayerName(target.source))
+                target.addMoney(amount)
+                TriggerClientEvent('esx:showNotification', target.source, 'You received ' .. amount .. '$ from ' .. GetPlayerName(source))
+            end
         else
             TriggerClientEvent('esx:showNotification', source, 'Invalid amount')
         end
